d8884dde15ccceb001c4c6f92abc29e2a6eed9cd
Justin Miles <justmiles94@gmail.com>
2018-03-20 19:51:34 -0500

    implemented about half of the impelemtn a custom control added pictures from not in xcode

---
+++ FoodTracker/FoodTracker/RatingController.swift
@@ -0,0 +1,69 @@
+//
+//  RatingController.swift
+//  FoodTracker
+//
+//  Created by Justin Miles on 3/20/18.
+//  Copyright © 2018 Justin Miles. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingController: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int =  5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton){
+        print("Button pressed")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons(){
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            button.addTarget(self, action: #selector(RatingController.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            //Add button to the stack
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
+    }
+}
--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     
     //Mark: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +33,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    //Mark: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss picker if user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        //Display photo
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     //Mark: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        //To pick a photo
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
     @IBOutlet weak var setDefaultLabelText: UIButton!
     @IBAction func setDefaultLabelText(_ sender: UIButton){
         mealNameLabel.text = "default Text"
